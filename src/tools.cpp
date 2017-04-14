e2e00761b8ab84005ad22aafd72a1d067c3ec900
Marco P. Nogueira <marco.nogueira@gatech.edu>
2017-04-13 21:10:37 -0400

    tools: +RMSE calculation utility function

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,29 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  //validate inputs
+  if ((estimations.size() == 0) || estimations.size() != ground_truth.size())
+  {
+    std::cout << "ERROR - invalid inputs to CalculateRMSE()!";
+    return rmse;
+  }
+
+  //squared residual accumulation
+  for (int i=0; i<estimations.size(); ++i)
+  {
+    VectorXd diff = estimations[i]-ground_truth[i];
+    diff = diff.array() * diff.array();
+    rmse += diff;
+  }
+
+  //calculate mean
+  rmse /= estimations.size();
+
+  //calculate square root
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
